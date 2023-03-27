db457983dbaae72a6d917cf8109f57d81c57070a
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 04:30:35 +0000

    Mar 27, 2023, 5:30 PM

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import requests
 api_key = "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"
 
 
+import os
+import openai
+openai.api_key = api_key
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
