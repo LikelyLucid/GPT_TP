1af6de262ecb03cc1276091c4d849f1ecbf0e9d5
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 03:33:24 +0000

    Mar 27, 2023, 4:33 PM

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import requests
-import random
+from proxylist import ProxyList
 proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
-req = requests.get(proxy_list_url)
 
-#print random line of txt from request
-print(req)
+pl = ProxyList.from_network_file(proxy_list_url)
+print(pl.get_random_server().get_address())
