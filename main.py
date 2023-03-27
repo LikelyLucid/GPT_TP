0e30093dde5a1d2410aaa7366c5d9d66eeb3eeca
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 03:49:32 +0000

    Mar 27, 2023, 4:49 PM

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from proxylist import ProxyList
 from revChatGPT.V1 import Chatbot
-
+import requests
 proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
 pl = ProxyList.from_network_file(proxy_list_url)
 proxy = pl.get_random_server().get_address()
 
-email = "micoolplays2@gmail.com"
-password = ""
+url = "https://chat.openai.com/api/auth/session"
+req = requests.get(url)
+print(req.text)
 
-GPT = Chatbot(config={"email": email, "password": password, "proxy": proxy})
+# GPT = Chatbot(config={"session_token" "proxy": proxy})
