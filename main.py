f7efe21e4c478ea9d2c0842f43ba77ee75496e03
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 03:30:32 +0000

    Mar 27, 2023, 4:30 PM

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import 
+import requests
+import random
+proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
+req = requests.get(proxy_list_url)
+
+#print random line of txt from request
+print(req)
