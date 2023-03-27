8b52e625d02f4d722d33bf83693e06f0c40428e5
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 03:34:00 +0000

    Mar 27, 2023, 4:34 PM

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from proxylist import ProxyList
 proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
-
 pl = ProxyList.from_network_file(proxy_list_url)
 print(pl.get_random_server().get_address())
