281f757bc2d580b1ef12860839bc2975fc61a0f0
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 04:19:00 +0000

    Mar 27, 2023, 5:19 PM

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import requests
 
 proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
 pl = ProxyList.from_network_file(proxy_list_url)
-proxy = f"https://{pl.get_random_server().get_address()}"
+proxy = f"http://{pl.get_random_server().get_address()}"
 print(proxy)
-email = "micool.plays@gmail.com"
+email = "micoolplays@gmail.com"
 password = "micooliscool"
 GPT = Chatbot(config={"email": email, "password": password, "proxy": proxy})
 prompt = "how many beaches does portugal have?"
