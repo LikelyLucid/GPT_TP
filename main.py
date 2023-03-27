e7e8e890dcf06a040c96afe0c3adf712ee3879a1
PurelyStupidity <micoolplays@gmail.com>
2023-03-27 03:43:00 +0000

    Mar 27, 2023, 4:43 PM

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from proxylist import ProxyList
+from revChatGPT.V1 import Chatbot
+
 proxy_list_url = "https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt"
 pl = ProxyList.from_network_file(proxy_list_url)
-print(pl.get_random_server().get_address())
+proxy = pl.get_random_server().get_address()
+
+
+
+GPT = Chatbot(config={})
